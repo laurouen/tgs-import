daf9d73a7cb61fe94ba3982e4da44df57d530013
laurouen <laurouen@gmail.com>
2019-06-22 18:32:34 +0200

    start dev cron2

--- cron-getprices.js
+++ cron-getprices.js
@@ -1,4 +1,15 @@
+var exec = require('child_process').exec;
+
 var cmd = 'wget -q https://donnees.roulez-eco.fr/opendata/instantane -O /home/tgs/import/cron_archives/PrixCarburants_instantane.zip';
-var child = exec(cmd, function(err) {
-  console.log('DONE');
-});
+var child = exec(
+    cmd,
+    function (error, stdout, stderr) {
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+        else {
+            console.log('ok -> unzip')
+        }
+    }
+  );
+  
