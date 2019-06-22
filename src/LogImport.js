776f2587ddd1c2c2ed3abb64e31168527b87de7a
laurouen <laurouen@gmail.com>
2019-06-22 19:48:10 +0200

    update cron file - fix typo logImportSchema

--- src/LogImport.js
+++ src/LogImport.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const logimport = new Schema({
+const logimportSchema = new Schema({
     name: String,
     dateZipCurled: Date,
 	dateImport2Bdd: Date,
 })
 
-const Logimport = mongoose.model('logimport', serviceSchema)
+const Logimport = mongoose.model('logimport', logimportSchema)
 
 module.exports = Logimport
