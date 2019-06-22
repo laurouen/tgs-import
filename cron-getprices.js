5dcbe403ff0f04e20d93669385559721396a0ccb
laurouen <laurouen@gmail.com>
2019-06-22 19:04:12 +0200

    update cron file - add unzip as import

--- cron-getprices.js
+++ cron-getprices.js
@@ -1,4 +1,5 @@
-var exec = require('child_process').exec;
+const exec = require('child_process').exec;
+const unzip = require('unzipper')
 
 const fileName = 'PrixCarburants_instantane'
 const zipExtension = '.zip'
