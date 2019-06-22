0c7750a0c5ae069bfd1e89df68807330561eca1f
laurouen <laurouen@gmail.com>
2019-06-22 15:29:31 +0200

    update cron file with curl command

--- cron-getprices.js
+++ cron-getprices.js
@@ -1,27 +1,4 @@
-const Path = require('path')  
-const Axios = require('axios')
-let fs = require('fs')
-//const urlFuelPricesZipFile = 'https://donnees.roulez-eco.fr/opendata/instantane'
-let zipFileDesitnation = 'cron_archives/last_imported_prices.zip'
-
-
-
-async function downloadImage () {  
-    const path = Path.resolve(__dirname, 'cron_archives', 'PrixCarburants_instantane.zip')
-  const writer = fs.createWriteStream(path)
-
-  const response = await Axios({
-    url: 'https://donnees.roulez-eco.fr/opendata/instantane',
-    method: 'GET',
-    responseType: 'arraybuffer'
-  })
-
-  response.data.pipe(writer)
-
-  return new Promise((resolve, reject) => {
-    writer.on('finish', resolve)
-    writer.on('error', reject)
-  })
-}
-
-downloadImage()  
+var cmd = 'wget -q https://donnees.roulez-eco.fr/opendata/instantane -O /home/tgs/import/cron_archives/PrixCarburants_instantane.zip';
+var child = exec(cmd, function(err) {
+  console.log('DONE');
+});
